082cb1306b4f8234f1d5128fa973cdcca8724f28
05750329011 <05750329011@senacrs.edu.br>
2024-07-12 21:57:12 -0300

    .

---
+++ Diagnostico/src/main/java/com/mycompany/diagnostico/Diagnostico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.diagnostico;
+
+/**
+ *
+ * @author 05750329011
+ */
+public class Diagnostico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Diagnostico/src/main/java/com/mycompany/diagnostico/doenca.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.diagnostico;
+
+/**
+ *
+ * @author 05750329011
+ */
+public class doenca {
+    String nome;
+    String definicao;
+    String sintomasString;
+    String tartamento;
+    String[] sintomas;
+}
